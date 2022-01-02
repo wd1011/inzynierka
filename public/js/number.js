1185f4eff64579f25e35444fa18957ffaa6f6e8c
wd1011 <wojtekdab15@gmail.com>
2022-01-02 17:17:01 +0100

    commit

---
+++ public/js/number.js
@@ -0,0 +1,26 @@
+const numberForm = document.querySelector('.number-road');
+const number = async(
+    droga,
+) => {
+    try {
+        const res = await axios({
+            method: 'POST',
+            url: '/remonty/znajdz/numerDrogi',
+            data: {
+                droga,
+            },
+        });
+        if (res.data.status === 'success') {
+            showAlert('success', 'Informacje Wysłane!');
+            console.log(droga)
+        }
+    } catch (err) {
+        showAlert('error', err.response.data.message);
+    }
+};
+if (numberForm)
+    numberForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const droga = document.getElementById('droga').value;
+        number(droga);
+    });
