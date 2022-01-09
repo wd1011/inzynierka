1fb5350ae64bc9893ab6994f032abe0c6b582a0f
wd1011 <wojtekdab15@gmail.com>
2022-01-09 22:50:29 +0100

    commit

--- controllers/roadsController.js
+++ controllers/roadsController.js
@@ -7,17 +7,15 @@ exports.getRoad = editModelHandler.getOne(Road);
 exports.createRoad = editModelHandler.createOne(Road);
 exports.deleteRoad = editModelHandler.deleteOne(Road);
 exports.updateRoad = editModelHandler.updateOne(Road);
-exports.getVoivodeship = editModelHandler.getByName(Road);
-exports.getTown = editModelHandler.getByName(Road);
-exports.getNumberOfRoad = editModelHandler.getByName(Road);
 
-exports.getAllRoads = catchAsync(async (req, res, next) => {
-  const features = new APIhandler(Road.find(), req.query).filter();
-  const road = await features.query;
 
-  Road.find({}, function (err, road) {
-    res.render('wyszukiwarka.ejs', {
-      roadList: road,
+exports.getAllRoads = catchAsync(async(req, res, next) => {
+    const features = new APIhandler(Road.find(), req.query).filter();
+    const road = await features.query;
+
+    Road.find({}, function(err, road) {
+        res.render('wyszukiwarka.ejs', {
+            roadList: road,
+        });
     });
-  });
 });
